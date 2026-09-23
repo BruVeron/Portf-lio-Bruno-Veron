41a33a9ba07e6982773e6108bfc9176d722f6e09
Bruno Veron <brunoveron12a.03.m@gmail.com>
2026-09-23 17:22:36 -0300

    teste

--- cadastro.js
+++ cadastro.js
@@ -41,7 +41,7 @@ function cadastrar() {
 
     // Envia os dados para o servidor
 
-    fetch("https://SEU-ENDERECO.onrender.com/api/cadastro", {
+    fetch("https://bruveron.github.io/Portf-lio-Bruno-Veron/cadastro.html", {
 
         method: "POST",
 
